ac0bc1027850c77163b5c6c032ec562828dcea10
TaeWonn <xodns9485@gmail.com>
2020-09-03 17:43:55 +0900

    mysql setting not build this source

--- src/main/java/com/tae/tae/TaeApplication.java
+++ src/main/java/com/tae/tae/TaeApplication.java
@@ -3,6 +3,7 @@ package com.tae.tae;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.tae")
 @EnableCaching
