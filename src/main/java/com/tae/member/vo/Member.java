8e44d582393986a428334fb7275cbd4e6d001fb7
tae <xodns5977@naver.com>
2020-10-22 22:58:49 +0900

    leaning EntityFactory

---
+++ src/main/java/com/tae/member/vo/Member.java
@@ -0,0 +1,28 @@
+package com.tae.member.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String name;
+    private Integer age;
+}
--- src/main/java/com/tae/tae/TaeApplication.java
+++ src/main/java/com/tae/tae/TaeApplication.java
@@ -3,7 +3,6 @@ package com.tae.tae;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.tae")
 @EnableCaching
@@ -12,5 +11,6 @@ public class TaeApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaeApplication.class, args);
     }
+
 
 }
