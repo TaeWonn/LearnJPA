cf656b5b8b13bb322d14d0f58bd622ea6245a922
TaeWonn <xodns9485@gmail.com>
2020-11-04 20:16:05 +0900

    IdClass를 활용한 복합 키 예제

---
+++ src/main/java/com/tae/tae/dto/inheritance/Child.java
@@ -0,0 +1,27 @@
+package com.tae.tae.dto.inheritance;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Child {
+
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "PARENT_ID1",
+                    referencedColumnName = "PARENT_ID1"),
+            @JoinColumn(name = "PARENT_ID2",
+                    referencedColumnName = "PARENT_ID2")
+    })
+    private Parent parent;
+}
---
+++ src/test/java/com/tae/tae/jpa/inheritance/ParentTest.java
@@ -0,0 +1,25 @@
+package com.tae.tae.jpa.inheritance;
+
+import com.tae.tae.dto.inheritance.Parent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ParentTest {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void Parent테스트(){
+        Parent parent = new Parent();
+        parent.setId1("myId1");
+        parent.setId2("myId2");
+        parent.setName("parentName");
+        em.persist(parent);
+    }
+}
